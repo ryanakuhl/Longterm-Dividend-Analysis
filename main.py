f10b510875f306998701857b61164181f65344eb
RKuhl <ryanakuhl@gmail.com>
2019-09-18 16:37:21 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import xlrd, requests, json, time
-import warnings
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
+import xlrd, requests, json, time, warnings
 
 book = xlrd.open_workbook("dividends.xlsx")
-print("Worksheet name(s): {0}".format(book.sheet_names()))
-ie_token = '****************************'
+ie_token = '************'
 
 class AllStock():
     def __init__(self, row, x):
@@ -23,6 +21,7 @@ class AllStock():
                 if int(self.PE_Ratio) in range(5, 40):
                     if self.Dividend_Yield > 3.75:
                         if self.Market_Cap > 10:
+                            #price filter
                             #if int(self.latestPrice) in range(4,12):
                             return True
 
@@ -54,12 +53,12 @@ def set_the_att(returned_dict):
         try:
             setattr(allstocks, a, returned_dict[a])
         except Exception as e:
-            print('line 61', e, returned_dict)
+            print('line 56', e, returned_dict)
 
-def advanced_research(allstocks):
+def advancing_research(allstocks):
     if allstocks.latestPrice > (allstocks.week52High * .9):
         pass
-    elif allstocks.latestPrice < (allstocks.week52Low * 1.1):
+    elif allstocks.latestPrice < (allstocks.week52Low * 1.4):
         pass
     elif int(round(allstocks.ytdChange*100)) not in range(0, 30):
         pass
@@ -77,20 +76,7 @@ for a in list(set(sh)):
                 if allstocks.funt_init_analyze():
                     set_the_att(allstocks.funt_stock_basic())
                     if hasattr(allstocks, 'week52Low') and type(allstocks.week52Low) is int or type(allstocks.week52Low) is float:
-                        if advanced_research(allstocks):
-                            """not available to free acounts
-                            #set_the_att(allstocks.funt_financials())
-                            if allstocks.netIncome / allstocks.totalAssets > .05:
-                                # Not broke
-                                if allstocks.operatingIncome - allstocks.operatingExpense < 0:
-                                    pass
-                                elif allstocks.currentAssets * 1.5 - allstocks.currentDebt < 0:
-                                    pass
-                                    # retain 10% profit
-                                elif allstocks.operatingRevenue / allstocks.operatingIncome < 0.1:
-                                    pass
-                                else:
-                            set_the_att(allstocks.funt_earnings_report())"""
+                        if advancing_research(allstocks):
                             set_the_att(allstocks.funt_dividends()[0])
                             print('Success ', allstocks.ticker, allstocks.latestPrice)
                             #get all attributes and values
@@ -108,7 +94,20 @@ for a in list(set(sh)):
                         
                             """
     except Exception as e:
-        print('line 115 ', a.row(rx)[0], e)
+        print('line 97 ', a.row(rx)[0], e)
     time.sleep(5)
 
-
+def used_to_be_free():
+    """not available to free acounts anymore"""
+    #set_the_att(allstocks.funt_financials())
+    if allstocks.netIncome / allstocks.totalAssets > .05:
+        # Not financially broken
+        if allstocks.operatingIncome - allstocks.operatingExpense < 0:
+            pass
+        elif allstocks.currentAssets * 1.5 - allstocks.currentDebt < 0:
+            pass
+            # retain 10% profit
+        elif allstocks.operatingRevenue / allstocks.operatingIncome < 0.1:
+            pass
+        else:
+    set_the_att(allstocks.funt_earnings_report())
